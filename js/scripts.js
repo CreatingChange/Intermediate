310ca9c1251392a01ede46e8f35b35225fa10ace
Brittany Winston <brittany@finedesigngroup.com>
2015-02-06 14:38:52 -0700

    it works

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,58 @@
 $(document).ready(function() {
-    $(".image_1").click(function() {
-        $("#click_image").show();
-        $(".hide_this").hide();
-    });
+  $(".image_1").click(function() {
+      $("#click_image").show();
+      $(".hide_this").hide();
+  });
 
-    $("#click_image").click(function() {
-        $(".hide_this").show();
-        $("#click_image").hide();
-    });
+  $("#click_image").click(function() {
+      $(".hide_this").show();
+      $("#click_image").hide();
+  });
 
-    $(".font-size-up").click(function() {
-        $(".container").css("font-size", function() {
-            return parseInt($(this).css('font-size')) + 1 + 'px';
-        });
-    });
+  $(".font-size-up").click(function() {
+      $(".container").css("font-size", function() {
+          return parseInt($(this).css('font-size')) + 1 + 'px';
+      });
+  });
 
-    $(".font-size-down").click(function() {
-        $(".container").css("font-size", function() {
-            return parseInt($(this).css('font-size')) - 1 + 'px';
-        });
-    });
+  $(".font-size-down").click(function() {
+      $(".container").css("font-size", function() {
+          return parseInt($(this).css('font-size')) - 1 + 'px';
+      });
+  });
 });
 
- 
 
-// function match_senator(state) {
-//   var senetors = {
-//     "OR" : "Jeff Merkley",
-//   };
-// }
 
-// $(function) {
-//   $('form#button').submit(function() {
-//     $('form').hide()
-//     var state = $(input).val()
-//     var lookup = match_senator(state)
-//     return false
-//   })
-// });
+$(function() {
+  $('.mail').click(function() {
+  $('form').hide();
+   Object.prototype.getKey = function(value){
+    for(var key in this){
+      if(this[key] == value){
+        return key;
+      }
+    }
+      return null;
+    };
+
+    var senators = {
+    "Jeff Merkley": 'OR',
+  };
+
+  var state = $('#state-code').val();
+  var key = senators.getKey(state);
+  console.log(key); // EL
+  $('#your_senator').append(key);
+  return false;
+  });
+});
+
+
+
+
+
+
 
 
 
