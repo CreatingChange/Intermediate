c601755ed2b0ff31672ce60a40b325944dcf0d7c
Brittany Winston <brittany@finedesigngroup.com>
2015-02-06 11:58:34 -0700

    fixes merge conflicts

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,23 @@ $(document).ready(function() {
         });
     });
 });
+
+ 
+
+function match_senator(state) {
+   var senetors = {
+    "OR" : "Jeff Merkley",
+  };
+}
+
+$(function) {
+  $('form#button').submit(function() {
+    $('form').hide()
+    var state = $(input).val()
+    var lookup = match_senator(state)
+    return false
+  })
+});
+
+
+
